1dc2f4572d6cf3dfd15ee84a85027a7e88d649d2
Andreas Heissenberger <andreas@heissenberger.at>
2018-03-29 10:35:07 +0200

    fixes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,15 +2,20 @@ import React from "react";
 
 const ProfileGrid = ({ members, bigger, prize }) => (
   <div className="row">
-    {members.map( (item,index) => (
-      <div className={"col-sm-12 col-md-6 "+(bigger?"col-lg-4":"col-lg-3")} key={index}>
+    {members.map(({ link, image, name, description }, index) => (
+      <div
+        className={"col-sm-12 col-md-6 " + (bigger ? "col-lg-4" : "col-lg-3")}
+        key={index}
+      >
         <div className="investors-card">
-          <span className="member-image">
-            <img src={item.image} alt="image" />
-          </span>
-          <h3>{item.name}</h3>
-          <p>{item.description}</p>
-          {prize?(<span className="invest-prize">$</span>):null}
+          <a href={link}>
+            <span className="member-image">
+              <img src={image} alt="image" />
+            </span>
+            <h3>{name}</h3>
+            <p>{description}</p>
+            {prize ? <span className="invest-prize">$</span> : null}
+          </a>
         </div>
       </div>
     ))}
--- src/templates/icoteam-section.js
+++ src/templates/icoteam-section.js
@@ -29,93 +29,25 @@ export const ICOTeamSectionTemplate = ({ team, investors, advisoryboard }) => {
           </div>
         </div>
       </div>
+      <div className="advisory-board">
+        <div className="container">
+          <div className="advisory-inner">
+            <h2></h2>
+            <h1>Partners &amp; Advisors</h1>
+            <ProfileGrid members={advisoryboard.members} bigger />
+          </div>
+        </div>
+      </div>
       <div className="sale-investors">
         <div className="container">
-          <h2>Our </h2>
-          <h1>Private-Sale Investors</h1>
+          <h2>CONDA </h2>
+          <h1>Equity Investors</h1>
           <ProfileGrid members={investors.members} bigger prize />
           <Reviews reviews={investors.quotes} />
           
         </div>
       </div>
-      <div className="advisory-board">
-        <div className="container">
-          <div className="advisory-inner">
-            <h2>Our</h2>
-            <h1>Advisory Board</h1>
-            <ProfileGrid members={advisoryboard.members} />
-          </div>
-        </div>
-      </div>
+      
     </section>
   );
 };
-
-// export default ({ data }) => {
-//   const { markdownRemark: post } = data;
-//   return (
-//     <ICOTeamSectionTemplate
-//       team={post.frontmatter.team}
-//       investors={post.frontmatter.investors}
-//       advisoryboard={post.frontmatter.advisoryboard}
-//     />
-//   );
-// };
-
-// export const ICOTeamSectionFragment = graphql`
-//   fragment ICOTeamDetails on MarkdownRemark {
-//     frontmatter {
-//       team {
-//         members {
-//           image
-//           name
-//           description
-//         }
-//       }
-//       investors {
-//         members {
-//           image
-//           name
-//           description
-//         }
-//       }
-//       advisoryboard {
-//         members {
-//           image
-//           name
-//           description
-//         }
-//       }
-//     }
-//   }
-// `;
-
-// export const ICOTeamSectionQuery = graphql`
-//   query ICOTeamSection($id: String!) {
-//     markdownRemark(id: { eq: $id }) {
-//       frontmatter {
-//         team {
-//           members {
-//             image
-//             name
-//             description
-//           }
-//         }
-//         investors {
-//           members {
-//             image
-//             name
-//             description
-//           }
-//         }
-//         advisoryboard {
-//           members {
-//             image
-//             name
-//             description
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
