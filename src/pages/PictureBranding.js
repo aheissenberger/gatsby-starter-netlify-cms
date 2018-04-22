a3fa4fabaf4d63dcd874e1095a8cc0193390a723
CodingYourLife <>
2018-04-22 23:50:21 +0200

    PictureBranding site created but empty

---
+++ src/pages/PictureBranding.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const PictureBranding = () => {
+    return (
+      <section
+        className="blog wow fadeInUp"
+        data-wow-duration="0.5s"
+        style={{
+          visibility: "visible",
+          animationDuration: "0.5s",
+          animationName: "fadeInUp"
+        }}>
+        <div className="container">
+          <div className="blog__header">
+            <h2>
+              Profile Picture Branding
+            </h2>
+          </div>
+          <div className="blog__element">
+            <div className="row">
+              Hello World
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+}
+
+export default PictureBranding
