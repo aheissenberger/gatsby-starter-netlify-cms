ae55ff8c56aac5c64c39e2f73fa1ce7cb432d0c6
Andreas Heissenberger <andreas@heissenberger.at>
2018-05-07 15:54:13 +0200

    add AFFID (Affiliate ID Saving to local storage)

--- static/custom.js
+++ static/custom.js
@@ -158,3 +158,21 @@ $(document).ready(function collapseOnClickFix(){
      }, 150);
   });
 });
+
+(function () {
+  function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    // eslint-disable-next-line
+    name = name.replace(/[\[\]]/g, "\\$&"); 
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+
+  var AFFID = getParameterByName('AFFID');
+  if (AFFID && typeof AFFID === 'string' && AFFID.length >=67 && AFFID.substr(0,3) === 'AFF' && window.localStorage) {
+    window.localStorage.setItem('AFFID',AFFID)
+  }
+})();
