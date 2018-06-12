5cc8b44bfee09ccbfd4c8779d386526682289119
CodingYourLife <johannes.mayer@conda.at>
2018-06-12 16:14:23 +0200

    video show hide fix for bootstrap4 (was bootstrap3 before and didn't work)

--- src/components/VideoDemonstration.js
+++ src/components/VideoDemonstration.js
@@ -5,10 +5,10 @@ const VideoDemonstration = ({ url, title, description }) => {
       <div className="video-demonstration">
         <div className="container">
           <div className="row">
-            <div className="col-sm-12 col-md-12 col-lg-6">
+            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6">
               <iframe width="400" height="225" src={url} frameBorder="0" allow="encrypted-media" allowFullScreen></iframe>
             </div>
-            <div className="hidden-xs hidden-md col-lg-6">
+            <div className="d-none d-lg-block col-lg-6">
               <div className="video-demonstration-inner-text">
                 <h2>{title}</h2>
                 <p>{description}</p>
